7ce688a1fa20e1fcd95614814c1c6aabdf1cab4a
timothy-quan <quanti@sheridancollege.ca>
2021-04-05 05:57:47 -0400

    First Commit

---
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Dealer.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author teddy
+ */
+public abstract class Dealer {
+    
+    private final String dealerID = "AI Dealer";
+    
+    /**
+     * @return the dealerID
+     */
+    public String getDealerID() 
+    {
+        return dealerID;
+    }
+    
+}

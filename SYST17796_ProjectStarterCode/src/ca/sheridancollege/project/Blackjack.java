4c98f0ed458fbac9b5e8c22579bfc4a87f765926
timothy-quan <quanti@sheridancollege.ca>
2021-04-05 05:57:25 -0400

    First Commit

---
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Blackjack.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author teddy
+ */
+public class Blackjack extends Game {
+    
+    private Dealer dealer;
+
+    public Blackjack(String givenName) {
+        super(givenName);
+    }
+    
+    public void play() {
+        
+    }
+    
+    public void declareWinner() {
+        
+    }
+    
+    public void changeBet() {
+        
+    }
+    
+    public void cashOut() {
+        
+    }
+    
+}

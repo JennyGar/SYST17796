1720198892e8c8f92dc08b637539df637e346cc1
timothy-quan <quanti@sheridancollege.ca>
2021-04-14 00:36:23 -0400

    Second Commit

--- SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Blackjack.java
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Blackjack.java
@@ -7,7 +7,7 @@ package ca.sheridancollege.project;
 
 /**
  *
- * @author teddy
+ * @author Timothy Quan
  */
 public class Blackjack extends Game {
     
@@ -31,6 +31,14 @@ public class Blackjack extends Game {
     
     public void cashOut() {
         
+    }
+    
+    public boolean isAlreadyRegistered(BlackjackPlayer player) {
+        return getPlayers().contains(player);
+    }
+    
+    public void registerPlayer(BlackjackPlayer player) {
+        getPlayers().add(player);
     }
     
 }

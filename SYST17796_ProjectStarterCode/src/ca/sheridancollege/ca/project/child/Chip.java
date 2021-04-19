33e4ddf95a7f5b01c3529f1f0c7ce4663a68bd2e
timothy-quan <quanti@sheridancollege.ca>
2021-04-18 23:50:48 -0400

    Third Commit

---
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/ca/project/child/Chip.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.ca.project.child;
+
+/**
+ *
+ * @author Timothy Quan
+ */
+public enum Chip {
+
+    FIVE_HUNDRED(500),
+    ONE_HUNDRED(100),
+    TWENTY_FIVE(25),
+    TEN(10),
+    FIVE(5),
+    ONE(1);
+
+    private int value;
+
+    private Chip(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+    
+}

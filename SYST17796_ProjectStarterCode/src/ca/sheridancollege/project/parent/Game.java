51b2de84c43e06aef6208137f60b7c33a015e75f
timothy-quan <74888687+timothy-quan@users.noreply.github.com>
2021-04-19 01:34:57 -0400

    Base class
    Added index (i) parameter to declareWinner()

--- SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/parent/Game.java
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/parent/Game.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
  * The class that models your game. You should create a more specific
  * child of this class and instantiate the methods given.
  * @author dancye, 2018
+ * @modifier Timothy Quan, 4/18/2021
  */
 public abstract class Game 
 {
